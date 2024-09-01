9f63a9e2eb1a617296139a92a109c984dcd85d77
vinojiniParamasivam <vinojini.p@viryaconsulting.com>
2024-09-01 20:19:42 +0530

    Implement order agnostic binary search algorithm

--- src/main/java/org/example/BinarySearch.java
+++ src/main/java/org/example/BinarySearch.java
@@ -2,8 +2,11 @@ package org.example;
 
 public class BinarySearch {
     public static void main(String[] args) {
-        int[] arr={21,34,54,56,65,67,78,99};
-        System.out.println("Ordinary binary search "+binarySearch(arr,214));
+        int[] arr = {21, 34, 54, 56, 65, 67, 78, 99};
+        int[] arrDesc = {99, 78, 67, 65, 56, 54, 34, 21};
+        int[] peakArr = {0, 8, 9, 10, 9};
+        System.out.println("Ordinary binary search " + binarySearch(arr, 54));
+        System.out.println("Order agnostic binary search " + orderAgnosticBinarySearch(arr, 54));
     }
 
     //    Ordinary binary search
@@ -18,6 +21,34 @@ public class BinarySearch {
                 start = mid + 1;
             } else {
                 return mid;
+            }
+        }
+        return -1;
+    }
+
+    //    Order agnostic binary search
+    private static int orderAgnosticBinarySearch(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+        boolean asc = arr[start] < arr[end];
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (arr[mid] == target) {
+                return mid;
+            }
+            if (asc) {
+                if (arr[mid] > target) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            } else {
+                if (arr[mid] > target) {
+                    start = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
             }
         }
         return -1;
