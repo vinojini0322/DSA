7c486c40f452913f7e1efece4dc50535a963272d
vinojiniParamasivam <vinojini.p@viryaconsulting.com>
2024-09-01 20:20:57 +0530

    Implement leetcode question 852 - Find peak element from mountain array

--- src/main/java/org/example/BinarySearch.java
+++ src/main/java/org/example/BinarySearch.java
@@ -7,6 +7,7 @@ public class BinarySearch {
         int[] peakArr = {0, 8, 9, 10, 9};
         System.out.println("Ordinary binary search " + binarySearch(arr, 54));
         System.out.println("Order agnostic binary search " + orderAgnosticBinarySearch(arr, 54));
+        System.out.println("Peak element in mountain array " + peakIndexInMountainArray(peakArr));
     }
 
     //    Ordinary binary search
@@ -52,5 +53,22 @@ public class BinarySearch {
             }
         }
         return -1;
+    }
+
+    //    Find peek element in mountain array
+    //    LeetCode question 852
+    public static int peakIndexInMountainArray(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+            if (arr[mid] > arr[mid + 1]) {
+                end = mid;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return start;
+
     }
 }
