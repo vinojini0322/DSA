28607e40514dc9b2966c2d1b65ae0ebde5d9daad
vinojiniParamasivam <vinojini.p@viryaconsulting.com>
2024-09-02 07:45:44 +0530

    Implement leetcode question 1095 - Find in mountain array

--- src/main/java/org/example/BinarySearch.java
+++ src/main/java/org/example/BinarySearch.java
@@ -75,7 +75,7 @@ public class BinarySearch {
 
     //    Peak element in an array
     //    LeetCode question 162
-    public int findPeakElement(int[] nums) {
+    public static int findPeakElement(int[] nums) {
         int start = 0;
         int end = nums.length - 1;
         while (start < end) {
@@ -90,4 +90,43 @@ public class BinarySearch {
         return start;
 
     }
+
+    //    Find the target in mountain array
+//    LeetCode question 1095
+        private static int findInMountainArray(int target, int[] mountainArr) {
+            int start = 0;
+            int end = mountainArr.length - 1;
+            int peak = findPeakElement(mountainArr);
+            int ans = binarySearch(mountainArr, target, start, peak, true);
+            if (ans != -1) {
+                return ans;
+            }
+            return binarySearch(mountainArr, target, peak + 1, end, false);
+        }
+
+        private static int binarySearch(int[] mountainArr, int target, int start, int end, boolean isAscending) {
+            while (start <= end) {
+                int mid = start + (end - start) / 2;
+                int midVal = mountainArr[mid];
+
+                if (midVal == target) {
+                    return mid;
+                }
+
+                if (isAscending) {
+                    if (midVal < target) {
+                        start = mid + 1;
+                    } else {
+                        end = mid - 1;
+                    }
+                } else {
+                    if (midVal > target) {
+                        start = mid + 1;
+                    } else {
+                        end = mid - 1;
+                    }
+                }
+            }
+            return -1;
+        }
 }
