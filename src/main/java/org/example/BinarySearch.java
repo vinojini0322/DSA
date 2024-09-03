3ddcfbc15c6e074218ffbf9cadd5fd27250d169e
vinojiniParamasivam <vinojini.p@viryaconsulting.com>
2024-09-03 10:20:47 +0530

    Implement algorithm to find the ceiling of a number

--- src/main/java/org/example/BinarySearch.java
+++ src/main/java/org/example/BinarySearch.java
@@ -9,6 +9,7 @@ public class BinarySearch {
         System.out.println("Order agnostic binary search " + orderAgnosticBinarySearch(arr, 54));
         System.out.println("Order agnostic binary search " + orderAgnosticBinarySearch(arrDesc, 54));
         System.out.println("Peak element in mountain array " + peakIndexInMountainArray(peakArr));
+        System.out.println("Find ceiling of number " + ceilingOfNumber(arr, 64));
     }
 
     //    Ordinary binary search
@@ -92,41 +93,60 @@ public class BinarySearch {
     }
 
     //    Find the target in mountain array
-//    LeetCode question 1095
-        private static int findInMountainArray(int target, int[] mountainArr) {
-            int start = 0;
-            int end = mountainArr.length - 1;
-            int peak = findPeakElement(mountainArr);
-            int ans = binarySearch(mountainArr, target, start, peak, true);
-            if (ans != -1) {
-                return ans;
-            }
-            return binarySearch(mountainArr, target, peak + 1, end, false);
+    //    LeetCode question 1095
+    private static int findInMountainArray(int target, int[] mountainArr) {
+        int start = 0;
+        int end = mountainArr.length - 1;
+        int peak = findPeakElement(mountainArr);
+        int ans = binarySearch(mountainArr, target, start, peak, true);
+        if (ans != -1) {
+            return ans;
         }
+        return binarySearch(mountainArr, target, peak + 1, end, false);
+    }
 
-        private static int binarySearch(int[] mountainArr, int target, int start, int end, boolean isAscending) {
-            while (start <= end) {
-                int mid = start + (end - start) / 2;
-                int midVal = mountainArr[mid];
+    private static int binarySearch(int[] mountainArr, int target, int start, int end, boolean isAscending) {
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            int midVal = mountainArr[mid];
 
-                if (midVal == target) {
-                    return mid;
-                }
+            if (midVal == target) {
+                return mid;
+            }
 
-                if (isAscending) {
-                    if (midVal < target) {
-                        start = mid + 1;
-                    } else {
-                        end = mid - 1;
-                    }
+            if (isAscending) {
+                if (midVal < target) {
+                    start = mid + 1;
                 } else {
-                    if (midVal > target) {
-                        start = mid + 1;
-                    } else {
-                        end = mid - 1;
-                    }
+                    end = mid - 1;
+                }
+            } else {
+                if (midVal > target) {
+                    start = mid + 1;
+                } else {
+                    end = mid - 1;
                 }
             }
-            return -1;
         }
+        return -1;
+    }
+
+    //    Find the ceiling of number
+    private static int ceilingOfNumber(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (arr[mid] > target) {
+                end = mid - 1;
+            } else if (arr[mid] < target) {
+                start = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return start;
+    }
+
 }
