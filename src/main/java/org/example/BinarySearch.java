124d288c8bfd307c8eb33e366aa0a8beb10e0f6e
vinojiniParamasivam <vinojini.p@viryaconsulting.com>
2024-09-03 10:31:04 +0530

    Implement algorithm to find the floor of a number

--- src/main/java/org/example/BinarySearch.java
+++ src/main/java/org/example/BinarySearch.java
@@ -10,6 +10,7 @@ public class BinarySearch {
         System.out.println("Order agnostic binary search " + orderAgnosticBinarySearch(arrDesc, 54));
         System.out.println("Peak element in mountain array " + peakIndexInMountainArray(peakArr));
         System.out.println("Find ceiling of number " + ceilingOfNumber(arr, 64));
+        System.out.println("Find floor of number " + floorOfNumber(arr, 64));
     }
 
     //    Ordinary binary search
@@ -147,6 +148,23 @@ public class BinarySearch {
             }
         }
         return start;
+    }
+
+    //    Find floor of a number
+    private static int floorOfNumber(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (arr[mid] > target) {
+                end = mid - 1;
+            } else if (arr[mid] < target) {
+                start = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return end;
     }
 
 }
