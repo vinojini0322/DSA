92374cc83d34170106c4f32dded08a497920170c
vinojiniParamasivam <vinojini.p@viryaconsulting.com>
2024-09-03 20:52:29 +0530

    Implement algorithm to find target in the infinite loop

---
+++ src/main/java/org/example/FindTargetInInfiniteLoop.java
@@ -0,0 +1,32 @@
+package org.example;
+
+public class FindTargetInInfiniteLoop {
+    public static void main(String[] args) {
+        int[] arr = {1, 3, 4, 6, 8, 9, 12, 14, 16, 18, 19, 21, 34, 54, 56, 65, 67, 78, 99};
+        System.out.println("Find target in the infinite loop " + findTargetInInfiniteLoop(arr, 78));
+    }
+
+    //    Find target in the infinite loop
+    private static int findTargetInInfiniteLoop(int[] arr, int target) {
+        int start = 0;
+        int end = 1;
+        while (end < arr.length && arr[end] < target) {
+            start = end;
+            end = end * 2;
+        }
+        if (end >= arr.length) {
+            end = arr.length - 1;
+        }
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (arr[mid] > target) {
+                end = mid - 1;
+            } else if (arr[mid] < target) {
+                start = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+}
